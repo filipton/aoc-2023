44e83bf17577497f8c72d1c2739e8f9e4852897e
filipton <filipton12@gmail.com>
2023-12-11 10:12:45 +0100

    feat: day11

---
+++ day11/src/main.rs
@@ -0,0 +1,105 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let map = expand_universe(input);
+    let galaxies = get_galaxies(&map, 2);
+    let pairs = get_all_pairs(&galaxies);
+
+    let mut sum = 0;
+    for ((x1, y1), (x2, y2)) in pairs {
+        sum += (x1 - x2).abs() + (y1 - y2).abs();
+    }
+    println!("Part 1: {}", sum);
+
+    let galaxies = get_galaxies(&map, 1000000);
+    let pairs = get_all_pairs(&galaxies);
+    let mut sum = 0;
+    for ((x1, y1), (x2, y2)) in pairs {
+        sum += (x1 - x2).abs() + (y1 - y2).abs();
+    }
+
+    println!("Part 2: {}", sum);
+}
+
+fn get_galaxies(map: &Vec<Vec<char>>, empty_space_factor: i128) -> Vec<(i128, i128)> {
+    let mut galaxies: Vec<(i128, i128)> = vec![];
+
+    let mut real_y = 0;
+    for y in 0..map.len() {
+        if map[y][0] == '@' {
+            real_y += empty_space_factor;
+            continue;
+        }
+
+        let mut real_x = 0;
+        for x in 0..map[0].len() {
+            if map[y][x] == '@' {
+                real_x += empty_space_factor;
+                continue;
+            }
+
+            if map[y][x] == '#' {
+                galaxies.push((real_x, real_y));
+            }
+
+            real_x += 1;
+        }
+
+        real_y += 1;
+    }
+
+    galaxies
+}
+
+fn expand_universe(input: &str) -> Vec<Vec<char>> {
+    let mut lines: Vec<String> = vec![];
+
+    for line in input.lines() {
+        let line = line.trim();
+
+        if !line.chars().any(|c| c != '.') {
+            // @ - special char that represents empty line
+            lines.push(line.replace(".", "@"));
+        } else {
+            lines.push(line.to_string());
+        }
+    }
+
+    let mut map = lines
+        .iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    for x in 0..map[0].len() {
+        let mut tmp_dots = 0;
+
+        for y in 0..map.len() {
+            if map[y][x] == '.' || map[y][x] == '@' {
+                tmp_dots += 1;
+            }
+        }
+
+        if tmp_dots == map.len() {
+            replace_column(&mut map, x, '@');
+        }
+    }
+
+    map
+}
+
+fn replace_column(lines: &mut Vec<Vec<char>>, column: usize, c: char) {
+    for line in lines {
+        line[column] = c;
+    }
+}
+
+fn get_all_pairs<T>(input: &[T]) -> Vec<(&T, &T)> {
+    let mut pairs = vec![];
+
+    for i in 0..input.len() {
+        for j in i + 1..input.len() {
+            pairs.push((&input[i], &input[j]));
+        }
+    }
+
+    pairs
+}
