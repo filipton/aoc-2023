67113639774448601792ca0f2045d11fbb4fa371
filipton <filipton12@gmail.com>
2023-12-11 11:38:27 +0100

    chore: little day11 cleanup

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let input = include_str!("input.txt");
     let map = expand_universe(input);
     let galaxies = get_galaxies(&map, 2);
-    let pairs = get_all_pairs(&galaxies);
+    let pairs = get_pairs(&galaxies);
 
     let mut sum = 0;
     for ((x1, y1), (x2, y2)) in pairs {
@@ -11,7 +11,7 @@ fn main() {
     println!("Part 1: {}", sum);
 
     let galaxies = get_galaxies(&map, 1000000);
-    let pairs = get_all_pairs(&galaxies);
+    let pairs = get_pairs(&galaxies);
     let mut sum = 0;
     for ((x1, y1), (x2, y2)) in pairs {
         sum += (x1 - x2).abs() + (y1 - y2).abs();
@@ -20,20 +20,20 @@ fn main() {
     println!("Part 2: {}", sum);
 }
 
-fn get_galaxies(map: &Vec<Vec<char>>, empty_space_factor: i128) -> Vec<(i128, i128)> {
+fn get_galaxies(map: &Vec<Vec<char>>, empty_space_size: i128) -> Vec<(i128, i128)> {
     let mut galaxies: Vec<(i128, i128)> = vec![];
 
     let mut real_y = 0;
     for y in 0..map.len() {
         if map[y][0] == '@' {
-            real_y += empty_space_factor;
+            real_y += empty_space_size;
             continue;
         }
 
         let mut real_x = 0;
         for x in 0..map[0].len() {
             if map[y][x] == '@' {
-                real_x += empty_space_factor;
+                real_x += empty_space_size;
                 continue;
             }
 
@@ -57,7 +57,7 @@ fn expand_universe(input: &str) -> Vec<Vec<char>> {
         let line = line.trim();
 
         if !line.chars().any(|c| c != '.') {
-            // @ - special char that represents empty line
+            // if line is only dots
             lines.push(line.replace(".", "@"));
         } else {
             lines.push(line.to_string());
@@ -86,13 +86,16 @@ fn expand_universe(input: &str) -> Vec<Vec<char>> {
     map
 }
 
-fn replace_column(lines: &mut Vec<Vec<char>>, column: usize, c: char) {
+fn replace_column<T>(lines: &mut Vec<Vec<T>>, column: usize, c: T)
+where
+    T: Copy,
+{
     for line in lines {
         line[column] = c;
     }
 }
 
-fn get_all_pairs<T>(input: &[T]) -> Vec<(&T, &T)> {
+fn get_pairs<T>(input: &[T]) -> Vec<(&T, &T)> {
     let mut pairs = vec![];
 
     for i in 0..input.len() {
